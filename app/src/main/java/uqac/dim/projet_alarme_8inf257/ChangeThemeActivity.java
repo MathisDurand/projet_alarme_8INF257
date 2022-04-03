beb3c08f7c13682011e38644005605f956da1c46
MathisDurand <mathis.durand@isen-ouest.yncrea.fr>
2022-04-03 05:02:39 +0200

    Ajouts divers

---
+++ app/src/main/java/uqac/dim/projet_alarme_8inf257/ChangeThemeActivity.java
@@ -0,0 +1,14 @@
+package uqac.dim.projet_alarme_8inf257;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class ChangeThemeActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.changetheme);
+    }
+}
