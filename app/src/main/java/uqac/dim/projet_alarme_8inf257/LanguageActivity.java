6806fcff81a5d6dba50f45a9c7690c274dba4dcb
MathisDurand <mathis.durand@isen-ouest.yncrea.fr>
2022-04-03 05:43:27 +0200

    Ajouts et link des pages entre elles

---
+++ app/src/main/java/uqac/dim/projet_alarme_8inf257/LanguageActivity.java
@@ -0,0 +1,59 @@
+package uqac.dim.projet_alarme_8inf257;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+public class LanguageActivity extends Activity {
+    TextView selectLangText;
+    Button btnFrench, btnEnglish, btn1 ,btn2;
+    Context context;
+    Resources resources;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lang_page);
+
+        // referencing the text and button views
+        selectLangText = findViewById(R.id.TitleTheme);
+        btnFrench = findViewById(R.id.RadioButtonFrench);
+        btnEnglish = findViewById(R.id.RadioButtonEnglish);
+        btn1 = findViewById(R.id.ButtonTheme1);
+        btn2 = findViewById(R.id.ButtonTheme2);
+        // setting up on click listener event over the button
+        // in order to change the language with the help of
+        // LocaleHelper class
+        btnEnglish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                context = LocaleHelper.setLocale(LanguageActivity.this, "en");
+                resources = context.getResources();
+                selectLangText.setText(resources.getString(R.string.selectLanguage));
+                btnEnglish.setText(resources.getString(R.string.radioEnglish));
+                btnFrench.setText(resources.getString(R.string.radioFrench));
+                btn1.setText(resources.getString(R.string.butonNameAlarms));
+                btn2.setText(resources.getString(R.string.butonNameParameters));
+            }
+        });
+
+        btnFrench.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                context = LocaleHelper.setLocale(LanguageActivity.this, "fr");
+                resources = context.getResources();
+                selectLangText.setText(resources.getString(R.string.selectLanguage));
+                btnEnglish.setText(resources.getString(R.string.radioEnglish));
+                btnFrench.setText(resources.getString(R.string.radioFrench));
+                btn1.setText(resources.getString(R.string.butonNameAlarms));
+                btn2.setText(resources.getString(R.string.butonNameParameters));
+            }
+        });
+
+    }
+}
--- app/src/main/java/uqac/dim/projet_alarme_8inf257/MainActivity.java
+++ app/src/main/java/uqac/dim/projet_alarme_8inf257/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,13 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnCreerAlarme = (Button) findViewById(R.id.main_to_creeralarme);
+        btnCreerAlarme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CreerAlarmActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btnParametre = (Button) findViewById(R.id.main_to_parametre);
+        btnParametre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Parametre.class);
+                startActivity(intent);
+            }
+        });
     }
-    public void parametre(View parametre) {
-        Intent intent = new Intent(MainActivity.this, Parametre.class);
-        startActivity(intent);
-    }
-    public void creeralarme(View creeralarme) {
-        Intent intent = new Intent(MainActivity.this, CreerAlarmActivity.class);
-        startActivity(intent);
-    }
+
 }
