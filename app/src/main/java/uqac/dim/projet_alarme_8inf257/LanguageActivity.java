0de8760a3409d1d52fe20f897caffcb703ec95ca
MathisDurand <mathis.durand@isen-ouest.yncrea.fr>
2022-05-03 17:27:09 +0200

    Langues fixed

--- app/src/main/java/uqac/dim/projet_alarme_8inf257/LanguageActivity.java
+++ app/src/main/java/uqac/dim/projet_alarme_8inf257/LanguageActivity.java
@@ -3,12 +3,15 @@ package uqac.dim.projet_alarme_8inf257;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.TextView;
+
+import java.util.Locale;
 
 public class LanguageActivity extends Activity {
     TextView selectLangText;
@@ -33,26 +36,28 @@ public class LanguageActivity extends Activity {
         btnEnglish.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                context = LocaleHelper.setLocale(LanguageActivity.this, "en");
-                resources = context.getResources();
-                selectLangText.setText(resources.getString(R.string.selectLanguage));
-                btnEnglish.setText(resources.getString(R.string.radioEnglish));
-                btnFrench.setText(resources.getString(R.string.radioFrench));
-                btn1.setText(resources.getString(R.string.butonNameAlarms));
-                btn2.setText(resources.getString(R.string.butonNameParameters));
+                Locale locale = new Locale("en");
+                Locale.setDefault(locale);
+                Resources resources = LanguageActivity.this.getResources();
+                Configuration config = resources.getConfiguration();
+                config.setLocale(locale);
+                resources.updateConfiguration(config, resources.getDisplayMetrics());
+                finish();
+                startActivity(getIntent());
             }
         });
 
         btnFrench.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                context = LocaleHelper.setLocale(LanguageActivity.this, "fr");
-                resources = context.getResources();
-                selectLangText.setText(resources.getString(R.string.selectLanguage));
-                btnEnglish.setText(resources.getString(R.string.radioEnglish));
-                btnFrench.setText(resources.getString(R.string.radioFrench));
-                btn1.setText(resources.getString(R.string.butonNameAlarms));
-                btn2.setText(resources.getString(R.string.butonNameParameters));
+                Locale locale = new Locale("fr");
+                Locale.setDefault(locale);
+                Resources resources = LanguageActivity.this.getResources();
+                Configuration config = resources.getConfiguration();
+                config.setLocale(locale);
+                resources.updateConfiguration(config, resources.getDisplayMetrics());
+                finish();
+                startActivity(getIntent());
             }
         });
 
